79fe0fb3a400d15ef1147a86159560614914300d
taka <neithi.gensin@gmail.com>
2023-06-29 23:56:42 +0900

    フロント環境構築作成(Reactとwebpack使用)

---
+++ invoice_front/src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export const App = () => {
+    return (
+        <div>
+            <h1>請求書アプリ</h1>
+            <p>フロント</p>
+            <a href="https://www.yahoo.co.jp/">YAHOOへ遷移</a>
+        </div>
+    );
+};
