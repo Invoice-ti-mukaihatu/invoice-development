ea405ceae81bc0ddbe5021ea506807cc60694155
taka <neithi.gensin@gmail.com>
2023-08-11 00:42:55 +0900

    ユニーク制約実装

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -1,31 +1,32 @@
 -- 1.ここがDBバーで使う、SQL文のファイル
--- 2.SQLエディタを開き、下記のSQL文をコピペして、「SQLスクリプトを実行する」をクリックすると、DBにテーブルが作成される
 CREATE TABLE `users` (
     -- テーブル名は複数形
     `id` bigint unsigned NOT NULL AUTO_INCREMENT,
     -- bigintは20桁までの整数
     `name` varchar(30) NOT NULL DEFAULT '',
     -- varcharは文字列
-    `mail` varchar(50) NOT NULL DEFAULT '',
+    `email` varchar(50) NOT NULL DEFAULT '',
     -- varcharは文字列
-    `password` varchar(50) NOT NULL DEFAULT '',
+    `password` varchar(100) NOT NULL DEFAULT '',
     -- NOT NULLは空白(空文字)はだめということ
     PRIMARY KEY (`id`) -- 主キー
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 -- 文字コードはutf8
 --  NOT NULLは空白(空文字)はだめということ
--- ここがAPIテスターで使う、POSTの部分になる（今回使う部分としては、"name","mail,"password"になる）
+-- ここがAPIテスターで使う、POSTの部分になる（今回使う部分としては、"name","email,"password"になる）
+-- ユニーク制約の追加
+ALTER TABLE
+    `users`
+ADD
+    CONSTRAINT `uk_users_email` UNIQUE (`email`);
+
 INSERT INTO
     `users` (
-        -- テーブル名は複数形
         `id`,
-        -- bigintは20桁までの整数
         `name`,
-        -- varcharは文字列
-        `mail`,
-        -- varcharは文字列
-        `password` -- NOT NULLは空白(空文字)はだめということ
+        `email`,
+        `password`
     )
 VALUES
     -- 主キーとは、テーブルの中で一意に決まる値のこと
