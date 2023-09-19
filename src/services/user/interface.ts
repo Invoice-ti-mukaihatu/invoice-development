00c784d12d8274de8a6a6b39033efe70138b7d43
kunrenyouAcount <1214kunrenyou@gmail.com>
2023-09-19 21:12:55 +0900

    更新処理を一部修正

--- src/services/user/interface.ts
+++ src/services/user/interface.ts
@@ -1,6 +1,6 @@
 export interface IUserService {
   // ログインするさ時のメソッド、正常時トークンを返す。エラーが発生した場合エラーを返す
-  checkForDuplicate(userId: number, email: string): Promise<boolean | null | Error>;
+  checkForDuplicate(userId: number, email: string): Promise<boolean | Error>;
   updateUser(
     userId: number,
     username: string,
